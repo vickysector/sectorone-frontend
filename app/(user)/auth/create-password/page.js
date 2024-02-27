794152e91e6d31609838f21ba6c9fa420117e793
robin sector <robbin@sector.co.id>
2024-02-27 13:59:32 +0700

    adjust the create-password page user height

--- app/(user)/auth/create-password/page.js
+++ app/(user)/auth/create-password/page.js
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Create password </h1>
-    </main>
-  );
+  return <main className="h-auth-screen bg-slate-500"></main>;
 }
