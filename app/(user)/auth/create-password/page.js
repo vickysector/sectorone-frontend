82308e19682201278d22526d91da3d453199ef79
robin sector <robbin@sector.co.id>
2024-02-27 17:20:32 +0700

    Agreement checkbox done

--- app/(user)/auth/create-password/page.js
+++ app/(user)/auth/create-password/page.js
@@ -6,14 +6,16 @@ export default function Home() {
     <main className="h-auth-screen -500 flex relative">
       <section className="flex-1 flex flex-col items-center justify-center h-full w-full ">
         <div className="">
-          <h1>Create password</h1>
-          <p>To get started, let’s set up a strong password for your account</p>
+          <h1 className="text-heading-1">Create password</h1>
+          <p className="text-LG-normal text-text-description mt-[8px] mb-8">
+            To get started, let’s set up a strong password for your account
+          </p>
 
           <form action="">
             <div className="bg-input-container relative">
               <input
                 type="password"
-                className=" w-full bg-input-container py-1.5 px-3 border-input-border border-2 rounded-lg"
+                className=" w-full bg-input-container py-1.5 px-3 border-input-border border-2 rounded-lg text-Base-normal"
                 placeholder="Password"
               />
               <EyeOutlined
@@ -24,13 +26,28 @@ export default function Home() {
             <div className=" mt-4 text-right relative">
               <input
                 type="password"
-                className="  w-full bg-input-container py-1.5 px-3 border-input-border border-2 rounded-lg"
+                className="  w-full bg-input-container py-1.5 px-3 border-input-border border-2 rounded-lg text-Base-normal"
                 placeholder="Retype password"
               />
               <EyeOutlined
                 className="absolute top-[50%] right-4 translate-y-[-50%]"
                 style={{ color: "#00000040" }}
               />
+            </div>
+
+            <div className="mt-4 mb-8 ">
+              <label for="agreements" className="flex items-center mr-1.5">
+                <input
+                  type="checkbox"
+                  name=""
+                  id="agreements"
+                  value="I agree to the Terms & Conditions and Privacy Policy"
+                  className="accent-primary-base bg-white text-Base-normal"
+                />
+                <p className="text-Base-normal ml-1.5">
+                  I agree to the Terms & Conditions and Privacy Policy
+                </p>
+              </label>
             </div>
           </form>
         </div>
