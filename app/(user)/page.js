f9d660a975c2272d78929612a63bbe32196f94d9
robin sector <robbin@sector.co.id>
2024-02-27 13:12:07 +0700

    reset style on user route group

--- app/(user)/page.js
+++ app/(user)/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Testing User</h1>
+    <main>
+      <h1>Testing root user layout</h1>
     </main>
   );
 }
