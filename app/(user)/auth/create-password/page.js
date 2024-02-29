e1c04a888c7d0c247ce8f4062cb6da846dd790a4
robin sector <robbin@sector.co.id>
2024-02-29 18:41:20 +0700

    refactor: Adjust Progress bar to reusable components

--- app/(user)/auth/create-password/page.js
+++ app/(user)/auth/create-password/page.js
@@ -13,6 +13,7 @@ import {
 import { CheckPasswordRequirements } from "@/app/_lib/PasswordRequirements";
 import { AuthButton } from "@/app/_ui/components/buttons/AuthButton";
 import Password from "@/app/_ui/components/inputs/Password";
+import { ProgressBar } from "@/app/_ui/components/utils/ProgressBar";
 
 export default function Home() {
   const [showPassword, setShowPassword] = useState(false);
@@ -34,6 +35,15 @@ export default function Home() {
   const { overallStrength } = CalculatePasswordStrength(password);
   const { strength_1, strength_2, strength_3, strength_4, strength_5 } =
     GetCalculatePasswordStrengthByNumber(overallStrength);
+
+  const strengths = {
+    strength_1,
+    strength_2,
+    strength_3,
+    strength_4,
+    strength_5,
+    passwordRequirement,
+  };
 
   const handlePasswordFocus = () => {
     setIsPasswordFocused(true);
@@ -87,40 +97,7 @@ export default function Home() {
                 placeholder={"Password"}
               />
             </div>
-            <div
-              className={clsx(
-                "w-full flex items-center justify-between mt-3",
-                passwordRequirement ? "block" : "hidden"
-              )}
-            >
-              <div className="w-[85%] h-3 bg-[#0000000F] rounded-lg">
-                <div
-                  className={clsx(
-                    " h-3 rounded-lg ",
-                    strength_1 && "w-[20%] bg-red-500",
-                    strength_2 && "w-[40%] bg-red-300",
-                    strength_3 && "w-[60%] bg-orange-400",
-                    strength_4 && "w-[80%] bg-blue-600",
-                    strength_5 && "w-[100%] bg-success"
-                  )}
-                ></div>
-              </div>
-              <p
-                className={clsx(
-                  strength_1 && " text-red-500",
-                  strength_2 && " text-red-300",
-                  strength_3 && " text-orange-400",
-                  strength_4 && " text-blue-600",
-                  strength_5 && " text-success"
-                )}
-              >
-                {strength_1 && "Bad"}
-                {strength_2 && "Weak"}
-                {strength_3 && "Medium"}
-                {strength_4 && "Good"}
-                {strength_5 && "Strong"}
-              </p>
-            </div>
+            <ProgressBar data={strengths} />
             <div className=" mt-4 text-right relative">
               <Password
                 showPassword={showRetypePassword}
