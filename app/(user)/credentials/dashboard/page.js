1eed8421a95e0f481ab366c4071e59f0ecda346e
robin sector <robbin@sector.co.id>
2024-03-03 19:29:30 +0700

    create new layout base for dashboard

---
+++ app/(user)/credentials/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function UserDashboardPage() {
+  return (
+    <>
+      <h1>testing</h1>
+    </>
+  );
+}
---
+++ app/(user)/credentials/layout.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <main className="">
+      <nav className="py-1.5 px-8 flex items-center justify-between">
+        <Image
+          src={"/images/sector_logo.png"}
+          alt="Logo Sector"
+          width={92}
+          height={38}
+        />
+        <div className="flex items-center">
+          <Image
+            src={"/images/sector_notification.svg"}
+            alt="Notif"
+            width={22}
+            height={22}
+            className="mr-5"
+          />
+          <Image
+            src={"/images/sector_avatar.svg"}
+            alt="Avatar Profile"
+            width={28}
+            height={28}
+          />
+        </div>
+      </nav>
+      <section className="bg-input-container flex">
+        <aside className="bg-blue-400 h-auth-screen w-[230px] flex-none "></aside>
+        <div className="bg-orange-600 flex-grow">{children}</div>
+      </section>
+    </main>
+  );
+}
