258fd3a613efe613ca553cd687976365f2fce5d7
robin sector <robbin@sector.co.id>
2024-03-05 12:20:36 +0700

    Implement Reusable component of Bar Chart

--- app/(user)/credentials/dashboard/page.js
+++ app/(user)/credentials/dashboard/page.js
@@ -1,94 +1,7 @@
 "use client";
 
 import ChangeUrlButton from "@/app/_ui/components/buttons/ChangeUrlButton";
-import { Bar } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-const options = {
-  plugins: {
-    legend: {
-      display: false,
-
-      position: "top",
-      align: "start",
-      labels: {
-        usePointStyle: true,
-        boxHeight: 11,
-        boxWidth: 25,
-        font: {
-          color: "#000000",
-          size: "16px",
-          lineHeight: "24px",
-          weight: "500",
-        },
-      },
-    },
-    layout: {
-      padding: {
-        top: 20, // Adjust the top padding as needed
-        bottom: 10, // Adjust the bottom padding as needed
-      },
-    },
-    afterFit: function (chart, opt) {
-      chart.plugins.register({
-        afterFit: function () {
-          this.height = this.height + 150;
-        },
-      });
-    },
-  },
-};
-
-const labels = [
-  "Jan",
-  "Feb",
-  "March",
-  "Apr",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sept",
-  "Okt",
-  "Nov",
-  "Dec",
-];
-
-const data = {
-  labels,
-  datasets: [
-    {
-      label: "Employees",
-      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
-      backgroundColor: "#FAAD14",
-      borderRadius: 5,
-    },
-    {
-      label: "Users",
-      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
-      backgroundColor: "#1677FF",
-      borderRadius: 5,
-    },
-  ],
-};
+import ChartBarVertical from "@/app/_ui/components/charts/ChartBarVertical";
 
 export default function UserDashboardPage() {
   return (
@@ -114,17 +27,7 @@ export default function UserDashboardPage() {
           <button>2024</button>
           <button>All (Employee & User)</button>
           <div className="border-2 border-input-border rounded-[16px] mt-4 p-8 flex justify-center items-center w-full relative">
-            <div className="absolute top-[32px] left-[32px] flex items-center">
-              {" "}
-              <span className="w-[16px] h-[16px] bg-orange-chart block rounded-[50%]"></span>{" "}
-              <p className="ml-3 text-LG-normal text-black">Employee</p>{" "}
-            </div>
-            <div className="absolute top-[32px] left-[150px] flex items-center">
-              {" "}
-              <span className="w-[16px] h-[16px] bg-blue-chart block rounded-[50%]"></span>{" "}
-              <p className="ml-3 text-LG-normal text-black">Users</p>{" "}
-            </div>
-            <Bar options={options} data={data} className="mt-12" />
+            <ChartBarVertical />
           </div>
         </div>
       </section>
