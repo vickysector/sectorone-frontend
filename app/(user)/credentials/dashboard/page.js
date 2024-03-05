438eacbb8089a4664bb7fd0f43756b18a97269cd
robin sector <robbin@sector.co.id>
2024-03-05 12:04:04 +0700

    Dashboard Overview: Total Compromised done (overall)

--- app/(user)/credentials/dashboard/page.js
+++ app/(user)/credentials/dashboard/page.js
@@ -1,8 +1,98 @@
+"use client";
+
 import ChangeUrlButton from "@/app/_ui/components/buttons/ChangeUrlButton";
+import { Bar } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  plugins: {
+    legend: {
+      display: true,
+
+      position: "top",
+      align: "start",
+      labels: {
+        usePointStyle: true,
+        boxHeight: 11,
+        boxWidth: 25,
+        font: {
+          color: "#000000",
+          size: "16px",
+          lineHeight: "24px",
+          weight: "500",
+        },
+      },
+    },
+    layout: {
+      padding: {
+        top: 20, // Adjust the top padding as needed
+        bottom: 10, // Adjust the bottom padding as needed
+      },
+    },
+    afterFit: function (chart, opt) {
+      chart.plugins.register({
+        afterFit: function () {
+          this.height = this.height + 150;
+        },
+      });
+    },
+  },
+};
+
+const labels = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Okt",
+  "Nov",
+  "Dec",
+];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Employees",
+      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+      backgroundColor: "#FAAD14",
+      borderRadius: 5,
+    },
+    {
+      label: "Users",
+      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+      backgroundColor: "#1677FF",
+      borderRadius: 5,
+    },
+  ],
+};
 
 export default function UserDashboardPage() {
   return (
-    <>
+    <main>
       <section>
         <h1 className="text-heading-2 text-black mb-4">Overview</h1>
         <div className="bg-white flex items-center p-12 rounded-xl">
@@ -18,6 +108,16 @@ export default function UserDashboardPage() {
           </div>
         </div>
       </section>
-    </>
+      <section className="mt-8">
+        <h1 className="text-heading-4 text-black ">Total compromised</h1>
+        <div className="p-8 bg-white border-input-border border-2 mt-4 rounded-[16px]">
+          <button>2024</button>
+          <button>All (Employee & User)</button>
+          <div className="border-2 border-input-border rounded-[16px] mt-4 p-8 flex justify-center items-center w-full">
+            <Bar options={options} data={data} />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
