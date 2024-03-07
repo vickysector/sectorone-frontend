17104f9829697002f22d96fe224eb32084bbf21b
robin sector <robbin@sector.co.id>
2024-03-07 11:31:39 +0700

    Dashboard Overview: top malware and antivirus done

--- app/_ui/components/charts/ChartBarHorizontal.js
+++ app/_ui/components/charts/ChartBarHorizontal.js
@@ -1,15 +1,24 @@
 import clsx from "clsx";
 
-export default function ChartBarHorizontal({ name, number }) {
+export default function ChartBarHorizontal({
+  name,
+  number,
+  isMalware,
+  isAntivirus,
+  isHighest,
+}) {
   return (
-    <div className={clsx(`flex items-center w-[${number}%]`)}>
-      <span
+    <div className={clsx(`flex items-center `)}>
+      <div
         className={clsx(
-          ` w-full rounded-tr-lg rounded-br-lg bg-blue-chart h-[28px]  flex items-center `
+          `w-[${number}%] rounded-tr-lg rounded-br-lg   h-[28px]  flex items-center `,
+          isAntivirus && "bg-blue-chart",
+          isMalware && "bg-gold-chart"
         )}
       >
-        <p className="ml-4 text-white text-SM-normal">{name}</p>
-      </span>
+        <div className={`absolute top-0 left-0 right-0   bottom-0 `}></div>
+        <p className={clsx("ml-4 text-white text-SM-normal")}>{name}</p>
+      </div>
       <p className="text-black text-SM-normal ml-3">{number}</p>
     </div>
   );
