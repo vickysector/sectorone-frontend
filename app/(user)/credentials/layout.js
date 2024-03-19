cc4aec1524167158ae3af088e5fc49a429f0cc5c
robin sector <robbin@sector.co.id>
2024-03-19 09:24:41 +0700

    Backlog done: Adjust the sidebar style

--- app/(user)/credentials/layout.js
+++ app/(user)/credentials/layout.js
@@ -2,7 +2,12 @@
 
 import Sidenav from "@/app/_ui/dashboard/Sidenav";
 import Image from "next/image";
-import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  LeftOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
 import { useState } from "react";
 import clsx from "clsx";
 
@@ -38,14 +43,14 @@ export default function DashboardLayout({ children }) {
           />
         </div>
       </nav>
-      <section className="bg-input-container flex">
+      <section className="bg-input-container flex relative">
         <aside
           className={clsx(
-            " h-auth-screen w-[260px] flex-none transition-all fixed left-0 bottom-0 bg-white z-10 ",
+            " h-auth-screen w-[260px] flex-none transition-all fixed left-0 bottom-0 bg-white z-10 border-r-2 border-r-input-border ",
             hide ? "w-[102px]" : "w-[260px] overflow-x-hidden overflow-y-hidden"
           )}
         >
-          <div className="mt-2 flex justify-end mr-5">
+          {/* <div className="mt-2 flex justify-end mr-5">
             <MenuFoldOutlined
               style={{ fontSize: "20px" }}
               className={clsx(
@@ -62,12 +67,28 @@ export default function DashboardLayout({ children }) {
               )}
               onClick={toggleHideIcon}
             />
-          </div>
+          </div> */}
           <Sidenav />
         </aside>
         <div
           className={clsx(
-            "flex-grow min-h-screen h-full  min-w-screen w-full fixed bg-input-container left-0 right-0 overflow-y-auto  pt-[100px] transition-all pr-[32px] pb-[64px]",
+            "bg-white border-2 border-input-border w-[38px] h-[38px] rounded-full absolute top-[208px] transition-all z-10 flex items-center justify-center cursor-pointer",
+            hide ? "left-[87px]" : "left-[15.9%]"
+          )}
+          onClick={toggleHideIcon}
+        >
+          <LeftOutlined
+            style={{ fontSize: "16px" }}
+            className={clsx("cursor-pointer", hide ? "hidden" : "visible")}
+          />
+          <RightOutlined
+            style={{ fontSize: "16px" }}
+            className={clsx("cursor-pointer", hide ? "visible" : "hidden")}
+          />
+        </div>
+        <div
+          className={clsx(
+            "flex-grow min-h-screen h-full  min-w-screen w-full fixed bg-input-container left-0 right-0 overflow-y-auto  pt-[75px] transition-all pr-[32px] pb-[64px]",
             hide ? "pl-[130px]" : "pl-[290px]"
           )}
         >
--- app/_ui/dashboard/Sidenav.js
+++ app/_ui/dashboard/Sidenav.js
@@ -4,7 +4,6 @@ export default function Sidenav() {
   return (
     <div className="h-full">
       <div className="pt-9 text-center px-5">
-        {/* <div className="bg-blue-500 w-[50px] h-[50px] rounded-full absolute top-0 right-[-50px]"></div> */}
         <NavLinks />
       </div>
     </div>
