5a38143bc4637a8c4cf320e53d314bd66be443c3
robin sector <robbin@sector.co.id>
2024-03-26 09:12:53 +0700

    Create new page for error not authorize

---
+++ app/(user)/error/not-authorize/page.js
@@ -0,0 +1,16 @@
+import { PrimaryButton } from "@/app/_ui/components/buttons/PrimaryButton";
+import { useRouter, redirect } from "next/navigation";
+
+export default function NotAuthorizePage() {
+  return (
+    <main className=" h-[100vh] flex items-center justify-center ">
+      <div className="text-center">
+        <h1 className="text-heading-3">OOPPS... Something Wrong!.</h1>
+
+        <div className="mt-8">
+          <PrimaryButton value={"Back to Login"} href={"/auth/login"} />
+        </div>
+      </div>
+    </main>
+  );
+}
