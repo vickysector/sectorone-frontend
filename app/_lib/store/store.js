6b76e54a913e9272849f4e5b357d4b19abd8bc34
robin sector <robbin@sector.co.id>
2024-04-01 05:59:39 +0700

    Create new Redux Store and Slice for Storing URL domain users

--- app/_lib/store/store.js
+++ app/_lib/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ChangeUrlReducer from "./features/Home/ChangeUrlSlice";
+import ChooseUrlReducer from "./features/Home/ChooseUrlSlice";
 
 export const store = configureStore({
   reducer: {
     changeUrl: ChangeUrlReducer,
+    chooseUrl: ChooseUrlReducer,
   },
 });
