3e5aa94df604b23ef242d137b4165a526dea1906
robin sector <robbin@sector.co.id>
2024-04-01 09:17:50 +0700

    STATE: Create new redux store for Breaches

--- app/_lib/store/store.js
+++ app/_lib/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ChangeUrlReducer from "./features/Home/ChangeUrlSlice";
 import ChooseUrlReducer from "./features/Home/ChooseUrlSlice";
+import BreachesSlices from "./features/Breaches/BreachesSlices";
 
 export const store = configureStore({
   reducer: {
     changeUrl: ChangeUrlReducer,
     chooseUrl: ChooseUrlReducer,
+    breaches: BreachesSlices,
   },
 });
