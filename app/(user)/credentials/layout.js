c532b9985b5281b572f1648e9417bcd289334ff9
robin sector <robbin@sector.co.id>
2024-04-03 16:00:28 +0700

    Adjust loading state For Activity-log in credentials/layout

--- app/(user)/credentials/layout.js
+++ app/(user)/credentials/layout.js
@@ -620,8 +620,32 @@ export default function DashboardLayout({ children }) {
       <div
         className={clsx(
           "fixed top-0 bottom-0 left-0 right-0 bg-[#000000B2] w-full z-40 flex items-center justify-center text-black ",
-          loadingCompromisedData ? "visible" : "hidden",
           loadingActivityLogData ? "visible" : "hidden"
+        )}
+      >
+        <div
+          className={clsx(
+            "rounded-xl bg-white p-[24px] w-[35%] text-center flex justify-center items-center flex-col"
+          )}
+        >
+          <div>
+            <Image
+              src={"/images/image_loadingState.svg"}
+              alt="search icon"
+              width={300}
+              height={300}
+            />
+          </div>
+          <h1 className="text-LG-strong mt-4">Scanning data</h1>
+          <p className="text-text-description text-Base-normal mt-4">
+            This will take a moment...
+          </p>
+        </div>
+      </div>
+      <div
+        className={clsx(
+          "fixed top-0 bottom-0 left-0 right-0 bg-[#000000B2] w-full z-40 flex items-center justify-center text-black ",
+          loadingCompromisedData ? "visible" : "hidden"
         )}
       >
         <div
