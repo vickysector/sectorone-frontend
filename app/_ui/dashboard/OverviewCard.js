a87532b25a50c6d7fc6e15f27489144cd7f7b029
robin sector <robbin@sector.co.id>
2024-04-17 09:29:57 +0700

    FIX BUGS: [Overview & Compromise] Summarize card

--- app/_ui/dashboard/OverviewCard.js
+++ app/_ui/dashboard/OverviewCard.js
@@ -4,9 +4,9 @@ export default function OverviewCard({ image, total, descriptions }) {
   return (
     <>
       <div className="bg-white border-2 border-input-border rounded-lg p-4 flex items-center w-full [&:not(:last-child)]:mr-4 ">
-        <Image alt="logo" src={image} width={70} height={70} />
+        <Image alt="logo" src={image} width={48} height={48} />
         <div className="ml-4">
-          <h1 className="text-black text-heading-1">{total}</h1>
+          <h1 className="text-black text-heading-2">{total}</h1>
           <p className="text-text-description text-LG-normal">{descriptions}</p>
         </div>
       </div>
