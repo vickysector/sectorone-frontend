3632ec2a05502f4ac596399c2a038c9de0dc58bb
robin sector <robbin@sector.co.id>
2024-04-23 23:50:59 +0700

    fix bugs: adjust logo size in auth/login

--- app/(user)/auth/layout.js
+++ app/(user)/auth/layout.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function UserAuthLayout({ children }) {
   return (
     <main>
-      <nav className="py-1.5 px-8 border-b-2 border-b-input-border">
+      <nav className="py-3 px-8 border-b-2 border-b-input-border">
         <Image
           src={"/images/SectorOne.png"}
           alt="Logo Sector"
-          width={92}
+          width={96}
           height={38}
         />
       </nav>
