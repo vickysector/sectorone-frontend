930016f199475f2a64854d27106175345cf368cd
robin sector <robbin@sector.co.id>
2024-05-16 20:19:13 +0700

    Adjust Change url button components

--- app/_ui/components/buttons/ChangeUrlButton.js
+++ app/_ui/components/buttons/ChangeUrlButton.js
@@ -4,7 +4,7 @@ export default function ChangeUrlButton({ children, disabled, onClick }) {
   return (
     <button
       className={clsx(
-        "py-2 px-4 bg-input-container border-input-border border-2 rounded-lg text-LG-normal text-primary-base ",
+        "py-1 px-3 bg-primary-base  rounded-lg text-LG-normal text-white  ",
         !disabled
           ? "cursor-not-allowed opacity-30"
           : "cursor-pointer opacity-100"
